b61ea2498127dee8822c9a013430232e3bb054a4
Sam Moffat <sam.moffat@hotmail.com>
2017-11-23 16:05:57 +0000

    Added input to front-end

--- js/parser.js
+++ js/parser.js
@@ -1,6 +1,8 @@
-function parser() {
-	$(
-	
-		);
-
+function test() {
+	$().button('toggle');
+	console.log("test");
 }
+
+$("input[name='demo_vertical']").TouchSpin({
+      verticalbuttons: true
+    });
