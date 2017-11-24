b02a7eb6cbb958dbe36d464bd51d5c5f59f582c0
Sam Moffat <sam.moffat@hotmail.com>
2017-11-24 11:31:37 +0000

    Worked on janky number wheel

--- js/parser.js
+++ js/parser.js
@@ -1,8 +1,29 @@
+
 function test() {
 	$().button('toggle');
 	console.log("test");
 }
 
-$("input[name='demo_vertical']").TouchSpin({
-      verticalbuttons: true
-    });
+
+
+var x=0;
+
+$(document).on('click', '.number-spinner button', function () {
+	x++;
+	if (x%2 == 1) {    
+		var btn = $(this),
+			oldValue = btn.closest('.number-spinner').find('input').val().trim(),
+			newVal = 0;
+		
+		if (btn.attr('data-dir') == 'up') {
+			newVal = parseInt(oldValue) + 1;
+		} else {
+			if (oldValue > 1) {
+				newVal = parseInt(oldValue) - 1;
+			} else {
+				newVal = 1;
+			}
+		}
+	}
+	btn.closest('.number-spinner').find('input').val(newVal);
+});
