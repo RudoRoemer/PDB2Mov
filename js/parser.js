6e4b60a7293f9f08b9b1d47aaa84d63bce4c73fc
Sam Moffat <sam.moffat@hotmail.com>
2017-11-27 16:49:20 +0000

    Began frontend parsing

--- js/parser.js
+++ js/parser.js
@@ -1,29 +1,42 @@
+(function($){
+    function EnableApply() {
+
+        var OriginalCaptcha = $('#careersForm').data('captchaText');
+        var userCapcha = $('#captchaText').val();
+        if (OriginalCaptcha == userCapcha) {
+            $('#careerbtn').removeAttr('disabled');
+        }
+        else {
+            $('#careerbtn').attr('disabled', 'disabled');
+        }
+    }
+
+    function RegisterCapcha() {
+        $("#captcha").html(''); //reset the generated captcha first
+        $("#captchaText").val('');
+        $("#careersForm").clientSideCaptcha({
+            input: "#captchaText",
+            display: "#captcha",
+        });            
+    }
+}(jQuery));
+
+var multi = false;
+
+function checks() {
+	if($("#threed"). prop("checked") == true) {
+		console.log("true");
+	} else {
+		console.log("false");
+	}
+}
 
 function test() {
 	$().button('toggle');
 	console.log("test");
 }
 
-
-
-var x=0;
-
-$(document).on('click', '.number-spinner button', function () {
-	x++;
-	if (x%2 == 1) {    
-		var btn = $(this),
-			oldValue = btn.closest('.number-spinner').find('input').val().trim(),
-			newVal = 0;
-		
-		if (btn.attr('data-dir') == 'up') {
-			newVal = parseInt(oldValue) + 1;
-		} else {
-			if (oldValue > 1) {
-				newVal = parseInt(oldValue) - 1;
-			} else {
-				newVal = 1;
-			}
-		}
-	}
-	btn.closest('.number-spinner').find('input').val(newVal);
-});
+function fadeOut() {
+	$('#foo').fadeOut();
+	$('#foo').fadeIn();
+}
